c88bc59f9e10b7a15950fe8d359551acad82d0b4
Yacine Ben Mhenni <yassoyacine0009@gmail.com>
2023-10-04 22:32:54 +0100

    Add a counter

---
+++ src/components/Comment.jsx
@@ -0,0 +1,15 @@
+// import React from "react"
+
+export default function Comment() {
+  // const [sendArray, setSendAarry]= React.useState([])
+  return (
+    <div className="pagee">
+      <div className="holderr">
+        <img className="pic" src="" />
+        <h4 className="Name"></h4>
+        <h3 className="Date"></h3>
+        <p className="comment-"></p>
+      </div>
+    </div>
+  );
+}
